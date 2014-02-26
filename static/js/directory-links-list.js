a64410b0230912f7fd4457331f7dfcb7e7567185
Paul Frazee <pfrazee@gmail.com>
2014-02-26 16:40:54 -0600

    frontend: added confirm on delete and refresh link todo

--- static/js/directory-links-list.js
+++ static/js/directory-links-list.js
@@ -1,8 +1,14 @@
 if (_session.isPageAdmin) {
+	$('.directory-links-list .refresh-link-btn').on('click', function() {
+		alert('todo');
+		return false;
+	});
+
 	$('.directory-links-list .remove-link-btn').on('click', function() {
 		var $link = $(this).parents('.directory-link');
 		var internal_id = $link.data('internal-id');
 		if (typeof internal_id == 'undefined') return false;
+		if (!confirm('Delete this link?')) return false;
 		$page.follow({ rel: 'item', id: internal_id }).DELETE()
 			.then(function() { $link.remove(); })
 			.fail(function(res) { alert('Unexpected error: '+res.status+' '+res.reason); });
