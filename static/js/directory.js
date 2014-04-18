d1fb759a3f3810fcb2f502db080727994a3dc85f
Paul Frazee <pfrazee@gmail.com>
2014-04-18 13:04:25 -0500

    added feed-item selection (close #28)

--- static/js/directory.js
+++ static/js/directory.js
@@ -1549,23 +1549,16 @@ var globals = require('../globals');
 
 module.exports = {
 	setup: function() {
-		if (globals.session.isPageAdmin) {
-			$('.directory-links-list .refresh-link-btn').on('click', function() {
-				alert('todo');
-				return false;
-			});
-
-			$('.directory-links-list .remove-link-btn').on('click', function() {
-				var $link = $(this).parents('.directory-link');
-				var internal_id = $link.data('internal-id');
-				if (typeof internal_id == 'undefined') return false;
-				if (!confirm('Delete this link. Are you sure?')) return false;
-				globals.pageUA.follow({ rel: 'item', _internal: true, id: internal_id }).DELETE()
-					.then(function() { $link.remove(); })
-					.fail(function(res) { alert('Unexpected error: '+res.status+' '+res.reason); });
-				return false;
-			});
-		}
+		$('.directory-links-list').on('click', function(e) {
+			var slotEl = local.util.findParentNode.byClass(e.target, 'directory-item-slot');
+			if (slotEl) {
+				if (!e.ctrlKey) {
+					// unselect current selection if ctrl isnot held down
+					$('.directory-links-list .selected').removeClass('selected');
+				}
+				slotEl.classList.add('selected');
+			}
+		});
 	}
 };
 },{"../globals":4}],12:[function(require,module,exports){
