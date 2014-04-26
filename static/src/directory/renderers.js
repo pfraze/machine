aa596f588bfc4b1de0802046c4675e0d11e51b41
Paul Frazee <pfrazee@gmail.com>
2014-04-26 12:45:44 -0500

    put fetchbox in top left; fixed plugin guis to top

--- static/src/directory/renderers.js
+++ static/src/directory/renderers.js
@@ -25,38 +25,20 @@ local.addServer('thing-renderer', function(req, res) {
 
 // Default renderer
 local.addServer('about-renderer', function(req, res) {
-	var method = (req.path == '/view') ? 'GET' : 'HEAD';
-	local.dispatch({ method: method, url: 'local://host.env/selection/0', Authorization: req.header('Authorization') })
+	local.HEAD({ url: 'local://host.env/selection/0', Authorization: req.header('Authorization') })
 		.always(function(res2) {
 			var selfLink = local.queryLinks(res2, 'self')[0];
 			res.writeHead(200, 'OK', {'Content-Type': 'text/html'});
 			var html = '';
-			if (req.path == '/config') {
-				var nrows = (selfLink) ? Object.keys(selfLink).length : 1;
-				html += '<p><a href="/">details</a> config <a href="/view">view</a></p>';
-				html += '<form method="POST" action="/config">'+
-					'<div class="form-group"><textarea class="form-control" rows="'+nrows+'" name="meta">'+
-						util.escapeHTML(util.serializeRawMeta(selfLink))+
-					'</textarea></div>'+
-					'<button type="submit" class="btn btn-primary">Update</button>'+
-					' <button type="submit" class="btn btn-default">Defeed</button>'+
-				'</form>';
-			} else if (req.path == '/view') {
-				var doc = (res2.body && typeof res2.body == 'object') ? JSON.stringify(res2.body) : res2.body;
-				html += '<p><a href="/">details</a> <a href="/config">config</a> view</p>';
-				html += '<pre>'+util.escapeHTML(doc)+'</pre>';
-			} else {
-				// html += '<p>details <a href="/config">config</a> <a href="/view">view</a></p>';
-				if (selfLink) {
-					if (selfLink.id) { html += '<small class="text-muted">ID</small> '+util.escapeHTML(selfLink.id)+'<br>'; }
-					if (selfLink.rel) {
-						html += '<small class="text-muted">TYPE</small> '+util.decorateReltype(selfLink.rel);
-						if (selfLink.type) { html += ' '+util.escapeHTML(selfLink.type); }
-						html += '<br>';
-					}
-					if (selfLink.href) { html += '<small class="text-muted">HREF</small> <a href="'+util.escapeHTML(selfLink.href)+'" target=_blank>'+util.escapeHTML(selfLink.href)+'</a><br>'; }
-					if (selfLink.created_at) { html += '<small class="text-muted">CREATED</small> '+((new Date(+selfLink.created_at)).toLocaleTimeString())+'<br>'; }
+			if (selfLink) {
+				if (selfLink.id) { html += '<small class="text-muted">ID</small> '+util.escapeHTML(selfLink.id)+'<br>'; }
+				if (selfLink.rel) {
+					html += '<small class="text-muted">TYPE</small> '+util.decorateReltype(selfLink.rel);
+					if (selfLink.type) { html += ' '+util.escapeHTML(selfLink.type); }
+					html += '<br>';
 				}
+				if (selfLink.href) { html += '<small class="text-muted">HREF</small> <a href="'+util.escapeHTML(selfLink.href)+'" target=_blank>'+util.escapeHTML(selfLink.href)+'</a><br>'; }
+				if (selfLink.created_at) { html += '<small class="text-muted">CREATED</small> '+((new Date(+selfLink.created_at)).toLocaleTimeString())+'<br>'; }
 			}
 			res.end(html);
 		});
